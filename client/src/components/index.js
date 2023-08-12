526735c63b926e7ac7aa122f32e74ccd7a1bb2c7
Darsh Shah <102041543+dbs-2012@users.noreply.github.com>
2023-08-12 12:05:29 +0530

    initial commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import ColorPicker from "./ColorPicker";
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+import AIPicker from './AIPicker'
+
+export { AIPicker, CustomButton, ColorPicker, FilePicker, Tab }
